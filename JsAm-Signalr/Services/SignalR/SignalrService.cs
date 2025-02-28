b4c2e59a82b1f1c69ab9e74fd8dcd11687357c5b
Joao Silva <jotal96@gmail.com>
2025-02-28 11:46:02 +0000

    criado novo metodo para mensagens privadas

--- JsAm-Signalr/Services/SignalR/SignalrService.cs
+++ JsAm-Signalr/Services/SignalR/SignalrService.cs
@@ -9,13 +9,13 @@ public class SignalrService : Hub
     {
         _logger = logger;
     }
-
+    
     
     public override async Task OnConnectedAsync()
     {
         // envia ao cliente o seu ID
         await Clients.Client(Context.ConnectionId)
-            .SendAsync("OnConnectedAsync", "Entraste e o teu ID é: " + Context.ConnectionId);
+            .SendAsync("OnConnectedAsyncPrivate", "Entraste e o teu ID é: " + Context.ConnectionId);
         
         await Clients.All
             .SendAsync("OnConnectedAsync", 
@@ -24,5 +24,9 @@ public class SignalrService : Hub
 
     public async Task BroadCast(string message){
         Clients.All.SendAsync("BroadCast", Context.ConnectionId+": "+message);
+    }
+    
+    public async Task BroadCastPrivate(string message, string userId){
+        Clients.Client(userId).SendAsync("BroadCastPrivate", "Mensagem privada de:"+ Context.ConnectionId+":"+message);
     }
 }
